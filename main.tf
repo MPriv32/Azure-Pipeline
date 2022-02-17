c6102fa460966930a276a11708bfef24f4b45c18
MITCHELL PRIVETT <lahc33@gmail.com>
2022-02-17 14:52:46 -0800

    modules

--- main.tf
+++ main.tf
@@ -11,4 +11,29 @@ resource "random_id" "prefix" {
   byte_length = 8
 }
 
+module "ec2" {
+  source = "./modules/ec2"
+  infra_env = var.infra_env
+  public_ssh_key = var.public_ssh_key
+}
+
+module "iam" {
+  source = "./modules/iam"
+  infra_env = var.infra_env
+}
+
+module "s3" {
+  source = "./modules/s3"
+  infra_env = var.infra_env
+}
+
+module "sg" {
+  source = "./modules/sg"
+  infra_env = var.infra_env
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+  infra_env = var.infra_env
+}
 
