829ee1ba756d65bc5295cebe470263403189fae1
MITCHELL PRIVETT <lahc33@gmail.com>
2022-02-17 23:29:51 -0800

    idk man

--- main.tf
+++ main.tf
@@ -13,27 +13,35 @@ resource "random_id" "prefix" {
 
 module "ec2" {
   source = "./modules/ec2"
+
   infra_env = var.infra_env
   public_ssh_key = var.public_ssh_key
+  allow_rdp = module.sg.allow_rdp.id
+  allow_winrm = module.sg.allow_winrm.id
 }
 
 module "iam" {
   source = "./modules/iam"
+
   infra_env = var.infra_env
 }
 
 module "s3" {
   source = "./modules/s3"
+
   infra_env = var.infra_env
 }
 
 module "sg" {
   source = "./modules/sg" 
+
   infra_env = var.infra_env
+  vpc_id = module.vpc.vpc_1.id
   }
 
 module "vpc" {
   source = "./modules/vpc"
+  
   infra_env = var.infra_env
 }
 
